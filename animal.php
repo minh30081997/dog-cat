91e3c90030312c9aa250e2a5c41589cdd2116727
Minh <minhmyn97@gmail.com>
2022-12-20 06:07:10 +0700

    Add feature show for class Animal

---
+++ animal.php
@@ -0,0 +1,20 @@
+<?php
+
+class Animal
+{
+    public $name;
+    public $age;
+    public $color;
+
+    public function __construct($name, $age, $color)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->color = $color;
+    }
+
+    public function show()
+    {
+        echo $this->name . ': ' . $this->age . ': ' . $this->color;
+    }
+}
